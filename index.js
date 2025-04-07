f4d51e70428370addfef27f7295fcaef1aa26f89
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-04-07 17:17:29 +0600

    google login functionality done

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middlewares
-app.use(cors());
+const corsOption = {
+  origin: ["http://localhost:5173","http://localhost:5174"],
+  credentials: true,
+};
+app.use(cors(corsOption));
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("green link server is running");
 });
-
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jxshq.mongodb.net/?appName=Cluster0`;
 
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -34,16 +34,34 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    const userCollection = client.db("green-link-db").collection("users");
+
+    //// storing user to db
+
+    app.post("/store-user-info", async (req, res) => {
+      const newUser = req.body;
+      const email = newUser?.email;
+      const query = { email };
+      const isExist = await userCollection.findOne(query);
+      if (isExist) {
+        return res.send({ message: "This user already exist" });
+      }
+      if (req?.query?.role) {
+        newUser.role = "volunteer";
+      }
+      newUser.createdAt = new Date().toISOString();
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
 
 app.listen(port, () => {
   console.log(`Green-Link server is running on port ${port}`);
